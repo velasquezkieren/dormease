50d7b3e2331f3f17beb4f11338e124a9cbc50133
velasquezkieren <velasquezkieren@gmail.com>
2024-03-13 13:33:43 +0800

    update navbar

--- navbar.php
+++ navbar.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
         .navbar {
             background-color: #FAF9F6;
@@ -51,6 +52,7 @@
 </head>
 
 <body>
+    <!-- nav bar -->
     <header>
         <nav class="navbar navbar-expand-lg fixed-top" style="height:80px;">
             <div class="container-fluid">
