742a0c900e2336a3756180181e61235283580f53
velasquezkieren <velasquezkieren@gmail.com>
2024-04-29 19:52:16 +0800

    modified:   pages/about.php

--- pages/about.php
+++ pages/about.php
@@ -12,7 +12,7 @@
         <div class="row">
             <div class="col-12 container p-5">
                 <h2 class="mb-2 text-center">About Us</h2>
-                <p class="lead">DormEase, a brainchild of Mark Oniel M. Binuya and Kieren V. Velasquez, emerges as a beacon of convenience in the realm of student housing. Developed as a capstone project, it stands as a testament to innovation and foresight. With a focused mission to simplify the often arduous task of dorm hunting, DormEase caters specifically to the students of Nueva Ecija University of Science and Technology. Through intuitive design and user-friendly features, it aims to streamline the process, empowering students to find their ideal accommodations effortlessly. DormEase embodies the spirit of efficiency and student-centricity, ensuring a smoother transition into university life.</p>
+                <p class="lead text-center">DormEase, a brainchild of Mark Oniel M. Binuya and Kieren V. Velasquez, emerges as a beacon of convenience in the realm of student housing. Developed as a capstone project, it stands as a testament to innovation and foresight. With a focused mission to simplify the often arduous task of dorm hunting, DormEase caters specifically to the students of Nueva Ecija University of Science and Technology. Through intuitive design and user-friendly features, it aims to streamline the process, empowering students to find their ideal accommodations effortlessly. DormEase embodies the spirit of efficiency and student-centricity, ensuring a smoother transition into university life.</p>
             </div>
         </div>
     </div>
