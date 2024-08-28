d5f3f4076286b86db0f45cf7faed1462964a8a04
velasquezkieren <velasquezkieren@gmail.com>
2024-08-28 17:01:41 +0800

    modified:   pages/find.php

--- pages/find.php
+++ pages/find.php
@@ -1,5 +1,7 @@
 <?php
-if (!isset($_SESSION['u_Email'])) {
+$is_logged_in = isset($_SESSION['u_Email']);
+
+if (!$is_logged_in) {
 ?>
     <section class="p-3 p-md-4 p-xl-5">
         <div class="container p-xl-5" style="margin-top: 100px;">
@@ -12,53 +14,85 @@ if (!isset($_SESSION['u_Email'])) {
             </div>
         </div>
     </section>
-<?php } else {
-?>
-    <div class="container">
-        <div class="row m-auto">
-            <div class="col">
-                <div class="input-group mt-5 pt-5 p-4">
-                    <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
-                    <datalist id="datalistOptions">
-                        <option value="Cabanatuan">
-                        <option value="Sta. Rosa">
-                        <option value="Sumacab">
-                    </datalist>
-                </div>
-            </div>
-            <div class="col">
-                <div class="input-group mt-5 pt-5">
-                    <select class="form-select" aria-label="Default select example">
-                        <option value="Sort By" selected disabled>Sort By</option>
-                        <option value="1">Price - Low to High</option>
-                        <option value="2">Price - High to Low</option>
-                        <option value="3">Rating - Low to High</option>
-                        <option value="4">Rating - High to Low</option>
-                    </select>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php
+} else {
+    // Fetch all dormitories from the database
+    $sql = "SELECT * FROM dormitory";
+    $dorms_query = mysqli_query($con, $sql);
 
-    <div class="container">
-        <div class="row mt-5">
-            <div class="col-lg-3 border-0 card mb-2">
-                <a href="property"><img src="img/sample.jpg" class="card-img-top" alt="..."></a>
-                <div class="card-body">
-                    <h5 class="card-title">Cabanatuan</h5>
-                    <h6 class="card-text"><i class="bi bi-geo-alt-fill"></i>FX49+J2M, Lungsod ng Cabanatuan, Nueva Ecija</h6>
-                    <p class="card-text">Starts at<br>$1,000</p>
+    if (mysqli_num_rows($dorms_query) > 0):
+    ?>
+        <div class="container">
+            <div class="row m-auto">
+                <div class="col">
+                    <div class="input-group mt-5 pt-5 p-4">
+                        <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+                        <datalist id="datalistOptions">
+                            <option value="Cabanatuan">
+                            <option value="Sta. Rosa">
+                            <option value="Sumacab">
+                        </datalist>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="input-group mt-5 pt-5">
+                        <select class="form-select" aria-label="Default select example">
+                            <option value="Sort By" selected disabled>Sort By</option>
+                            <option value="1">Price - Low to High</option>
+                            <option value="2">Price - High to Low</option>
+                            <option value="3">Rating - Low to High</option>
+                            <option value="4">Rating - High to Low</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <?php while ($dorm = mysqli_fetch_assoc($dorms_query)): ?>
                     <?php
-                    if (isset($_SESSION['u_Account_Type']) && $_SESSION['u_Account_Type'] == 1) {
-                        echo '<a href="property" class="btn btn-dark">View Property</a>';
-                    } elseif (isset($_SESSION['u_Account_Type']) && $_SESSION['u_Account_Type'] == 0) {
-                        echo '<a href="property" class="btn btn-dark">Edit Property</a>';
+                    // Fetch the owner's name
+                    $owner_ID = mysqli_real_escape_string($con, $dorm['d_Owner']);
+                    $owner_query = mysqli_query($con, "SELECT u_FName, u_LName FROM user WHERE u_ID = '$owner_ID'");
+                    $owner_data = mysqli_fetch_assoc($owner_query);
+                    $owner_name = $owner_data ? htmlspecialchars($owner_data['u_FName'] . ' ' . $owner_data['u_LName']) : 'Unknown';
+
+                    // Get the image names and use the first image for the card
+                    $images = explode(',', $dorm['d_PicName']);
+                    $first_image = $images[0];
+
+                    // Limit the description to 100 characters
+                    $description = substr($dorm['d_Description'], 0, 100);
+                    if (strlen($dorm['d_Description']) > 100) {
+                        $description .= '...';
                     }
                     ?>
-
-                </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12 mb-2">
+                        <div class="card h-100 border-1">
+                            <a href="property?d_ID=<?= urlencode($dorm['d_ID']); ?>">
+                                <div class="card-img-container">
+                                    <img src="upload/<?= htmlspecialchars($first_image); ?>" class="card-img-top" alt="<?= htmlspecialchars($dorm['d_Name']); ?>">
+                                </div>
+                            </a>
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title"><?= htmlspecialchars($dorm['d_Name']); ?></h5>
+                                <p class="card-text text-truncate" style="max-height: 3.6em; overflow: hidden;"><?= htmlspecialchars($description); ?></p>
+                                <p class="card-text"><i class="bi bi-geo-alt-fill"></i> <?= htmlspecialchars($dorm['d_Street']) . ', ' . htmlspecialchars($dorm['d_City']); ?></p>
+                                <p class="card-text"><strong>Owner:</strong> <?= htmlspecialchars($owner_name); ?></p>
+                                <a href="property?d_ID=<?= urlencode($dorm['d_ID']); ?>" class="btn btn-dark mt-auto">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
             </div>
         </div>
-    </div>
-
-<?php } ?>
+    <?php
+    else:
+    ?>
+        <div class="container">
+            <div class="alert alert-secondary text-center mx-auto p-5" role="alert">
+                No listings available at the moment
+            </div>
+        </div>
+<?php endif;
+}
+?>
