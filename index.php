ead7c92de1bbb1a04d5706fe5b283121212a31b7
velasquezkieren <velasquezkieren@gmail.com>
2024-09-06 13:25:12 +0800

    new file:   css/pin-icon.svg 	modified:   index.php 	modified:   pages/listing.php

--- index.php
+++ index.php
@@ -35,12 +35,13 @@ switch ($page) {
     <title><?php echo $title; ?></title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <script src="scripts/leaflet.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <script src="scripts/jquery-3.7.1.min.js"></script>
     <link rel="icon" type="image/x-icon" href="./img/favicon.png">
-    <link rel="stylesheet" href="css/leaflet.css">
 
     <!-- style for navbar -->
     <style>
@@ -129,7 +130,7 @@ switch ($page) {
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- pages/listing.php
+++ pages/listing.php
@@ -16,8 +16,6 @@ if (isset($_POST['submit'])) {
     $d_ZIPCode = mysqli_real_escape_string($con, $_POST['d_ZIPCode']);
     $d_Province = mysqli_real_escape_string($con, $_POST['d_Province']);
     $d_Region = mysqli_real_escape_string($con, $_POST['d_Region']);
-    $d_Latitude = mysqli_real_escape_string($con, $_POST['d_Latitude']);
-    $d_Longitude = mysqli_real_escape_string($con, $_POST['d_Longitude']);
     $d_Description = mysqli_real_escape_string($con, $_POST['d_Description']);
     $d_Availability = '1';  // Default availability
     $d_Owner = $_SESSION['u_ID'];  // Assuming the user ID is stored in session
@@ -46,12 +44,11 @@ if (isset($_POST['submit'])) {
     $d_PicNames = implode(',', $imageNames);
 
     // Insert dormitory details into the database
-    $sql = "INSERT INTO dormitory (d_ID, d_Name, d_Street, d_City, d_ZIPCode, d_Province, d_Region, d_Latitude, d_Longitude, d_Availability, d_Description, d_Owner, d_PicName)
-            VALUES ('$d_ID', '$d_Name', '$d_Street', '$d_City', '$d_ZIPCode', '$d_Province', '$d_Region', '$d_Latitude', '$d_Longitude', '$d_Availability', '$d_Description', '$d_Owner', '$d_PicNames')";
+    $sql = "INSERT INTO dormitory (d_ID, d_Name, d_Street, d_City, d_ZIPCode, d_Province, d_Region, d_Availability, d_Description, d_Owner, d_PicName)
+            VALUES ('$d_ID', '$d_Name', '$d_Street', '$d_City', '$d_ZIPCode', '$d_Province', '$d_Region', '$d_Availability', '$d_Description', '$d_Owner', '$d_PicNames')";
 
     if (mysqli_query($con, $sql)) {
-        header("Location: property?d_ID=" . $d_ID);
-        exit();
+        header("location: property?d_ID=" . $d_ID);
     } else {
         echo "<script>(`Error: " . mysqli_error($con) . "`); </script>";
     }
@@ -76,91 +73,70 @@ if (!isset($_SESSION['u_Email'])) {
 <?php
 } else {
 ?>
+    <!-- form section -->
     <section class="p-3 p-md-4 p-xl-5 bg-light-subtle">
         <div class="container" style="margin-top:100px;">
             <div class="row justify-content-center">
                 <div class="col-12 col-xxl-11" id="list-property">
-                    <h2 class="h2 mb-4">Tell us about your property!</h2>
+                    <?php
+                    if (isset($_GET['edit'])) {
+                        echo '<h2 class="h2 mb-4">Edit Property</h2>';
+                    } else {
+                        echo '<h2 class="h2 mb-4">Tell us about your property!</h2>';
+                    }
+                    ?>
                     <div class="card border-light-subtle shadow-sm">
                         <div class="row g-0">
                             <div class="col-12">
                                 <div class="card-body p-3 p-md-4 p-xl-5">
                                     <form action="" class="row g-3" enctype="multipart/form-data" method="post">
-                                        <ul class="nav nav-tabs">
-                                            <li class="nav-item">
-                                                <a class="nav-link active_tab1" id="list_dorm_name" href="#dorm_name" data-toggle="tab">Dorm Name</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link inactive_tab1" id="list_dorm_details" href="#dorm_details" data-toggle="tab">Dorm Details</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link inactive_tab1" id="list_images" href="#images" data-toggle="tab">Images</a>
-                                            </li>
-                                        </ul>
-                                        <hr style="border: 1px solid black;">
-
-                                        <div class="tab-content">
-                                            <!-- Dorm Name Tab -->
-                                            <div class="tab-pane active" id="dorm_name">
-                                                <div class="col-12">
-                                                    <div class="form-floating mb-3">
-                                                        <input type="text" name="d_Name" class="form-control" id="floatingInput" placeholder="Dorm Name" required>
-                                                        <label for="floatingInput">Dorm Name</label>
-                                                    </div>
-                                                </div>
-                                                <button type="button" class="btn btn-primary btn-sm" id="btn_dorm_name">Next</button>
+                                        <div class="col-12">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="d_Name" class="form-control" id="floatingInput" placeholder="Dorm Name" required>
+                                                <label for="floatingInput">Dorm Name</label>
                                             </div>
+                                        </div>
 
-                                            <!-- Dorm Details Tab -->
-                                            <div class="tab-pane" id="dorm_details">
-                                                <div class="col-12">
-                                                    <div class="form-floating mb-3">
-                                                        <input type="text" name="d_Street" class="form-control" id="d_Street" placeholder="Street Address" required>
-                                                        <label for="d_Street">Street Address</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-12">
-                                                    <div class="form-floating mb-3">
-                                                        <input type="text" name="d_City" class="form-control" id="d_City" placeholder="City" required>
-                                                        <label for="d_City">City</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <div class="form-floating mb-3">
-                                                        <input type="number" name="d_ZIPCode" class="form-control" id="d_ZIPCode" placeholder="ZIP Code" required>
-                                                        <label for="d_ZIPCode">ZIP Code</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <div class="form-floating mb-3">
-                                                        <input type="text" name="d_Province" class="form-control" id="d_Province" placeholder="Province" required>
-                                                        <label for="d_Province">Province</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <div class="form-floating mb-3">
-                                                        <input type="text" name="d_Region" class="form-control" id="d_Region" placeholder="Region" required>
-                                                        <label for="d_Region">Region</label>
-                                                    </div>
-                                                </div>
-                                                <!-- Map for location selection -->
-                                                <div class="col-12">
-                                                    <div id="map" style="width: 100%; height: 400px;"></div>
-                                                    <input type="hidden" id="latitude" name="d_Latitude">
-                                                    <input type="hidden" id="longitude" name="d_Longitude">
-                                                </div>
-                                                <button type="button" class="btn btn-info btn-sm" id="previous_btn_dorm_details">Previous</button>
-                                                <button type="button" class="btn btn-primary btn-sm" id="btn_dorm_details">Next</button>
+                                        <div class="col-12">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="d_Street" class="form-control" id="d_Street" placeholder="Street Address" required>
+                                                <label for="d_Street">Street Address</label>
+                                                <div id="map" style="height: 450px; width:450px;"></div>
                                             </div>
-
-                                            <!-- Images Tab -->
-                                            <div class="tab-pane" id="images">
-                                                <div class="col-12">
-                                                    <input type="file" name="d_PicName[]" accept="image/*" multiple required>
-                                                </div>
-                                                <button type="button" class="btn btn-info btn-sm" id="previous_btn_images">Previous</button>
-                                                <button type="submit" class="btn btn-dark" name="submit">Submit</button>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="d_City" class="form-control" id="d_City" placeholder="City" required>
+                                                <label for="d_City">City</label>
                                             </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-floating mb-3">
+                                                <input type="number" name="d_ZIPCode" class="form-control" id="d_ZIPCode" placeholder="ZIP Code" required>
+                                                <label for="d_ZIPCode">ZIP Code</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="d_Province" class="form-control" id="d_Province" placeholder="Province" required>
+                                                <label for="d_Province">Province</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="d_Region" class="form-control" id="d_Region" placeholder="Region" required>
+                                                <label for="d_Region">Region</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <label for="d_Description" class="form-label">Property Description</label>
+                                            <textarea class="form-control" name="d_Description" id="d_Description" rows="5" required></textarea>
+                                        </div>
+                                        <div class="col-12">
+                                            <input type="file" name="d_PicName[]" accept="image/*" multiple required>
+                                        </div>
+                                        <div class="col-12">
+                                            <input type="submit" value="Submit" name="submit" class="btn btn-dark">
                                         </div>
                                     </form>
                                 </div>
@@ -171,55 +147,42 @@ if (!isset($_SESSION['u_Email'])) {
             </div>
         </div>
     </section>
+    <script>
+        // Initialize Leaflet map
+        var map = L.map('map', {
+            dragging: true
+        }).setView([15.4443, 120.9441], 20); // Cabanatuan, Nueva Ecija
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        // Custom Marker Icon
+        var customIcon = L.icon({
+            iconUrl: './css/marker-icon-2x.png', // Replace with your custom icon path
+            iconSize: [25, 41], // size of the icon
+            iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
+            popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+            shadowUrl: './css/marker-shadow.png', // optional shadow image
+            shadowSize: [50, 64], // size of the shadow
+            shadowAnchor: [4, 62] // the same for the shadow
+        });
+
+        // Add the marker at the center of the screen, but we'll update its position dynamically
+        var marker = L.marker(map.getCenter(), {
+            icon: customIcon
+        }).addTo(map);
+
+        // Update marker position based on map center when map is moved
+        map.on('move', function() {
+            var center = map.getCenter();
+            marker.setLatLng(center); // Set marker position to the map center
+            document.getElementById('latitude').value = center.lat;
+            document.getElementById('longitude').value = center.lng;
+        });
+    </script>
+
+
 <?php
 }
 ?>
-
-<script>
-    // Initialize Leaflet map
-    var map = L.map('map', {
-        dragging: true
-    }).setView([15.4443, 120.9441], 20); // Cabanatuan, Nueva Ecija
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-
-    // Custom Marker Icon
-    var customIcon = L.icon({
-        iconUrl: './css/marker-icon-2x.png', // Replace with your custom icon path
-        iconSize: [25, 41], // size of the icon
-        iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
-        popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
-        shadowUrl: './css/marker-shadow.png', // optional shadow image
-        shadowSize: [50, 64], // size of the shadow
-        shadowAnchor: [4, 62] // the same for the shadow
-    });
-
-    // Add the marker at the center of the screen, but we'll update its position dynamically
-    var marker = L.marker(map.getCenter(), {
-        icon: customIcon
-    }).addTo(map);
-
-    // Update marker position based on map center when map is moved
-    map.on('move', function() {
-        var center = map.getCenter();
-        marker.setLatLng(center); // Set marker position to the map center
-        document.getElementById('latitude').value = center.lat;
-        document.getElementById('longitude').value = center.lng;
-    });
-
-    // Form navigation script
-    $('#btn_dorm_name').click(function() {
-        $('.nav-tabs a[href="#dorm_details"]').tab('show');
-    });
-    $('#btn_dorm_details').click(function() {
-        $('.nav-tabs a[href="#images"]').tab('show');
-    });
-    $('#previous_btn_dorm_details').click(function() {
-        $('.nav-tabs a[href="#dorm_name"]').tab('show');
-    });
-    $('#previous_btn_images').click(function() {
-        $('.nav-tabs a[href="#dorm_details"]').tab('show');
-    });
-</script>
